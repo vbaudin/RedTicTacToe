2776274214f67ad794f827c3f853a75e041190a3
Vincent BAUDIN <vbaudin@e2r7p21.42.fr>
2018-05-31 15:46:35 +0200

    Game finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { withStateHandlers } from 'recompose';
-import { times } from 'ramda'
+import { map , times , filter } from 'ramda'
 import Popup from 'react-popup';
 import './App.css';
 
-const App = ({ isOpen, turn, clicked, board, checkWinner }) => {
+const App = ({ isOpen, turn, clicked, board, checkWinner, gameEnded }) => {
   return (
       <div id="game">
         <div id="head">
           RedTicTacToe
         </div>
-        <div id="board" onClick={(e) => clicked(e, turn, board, checkWinner)}>
+        <div id="board" onClick={(e) => gameEnded === false ? clicked(e, turn, board, checkWinner, gameEnded) : 0}>
           {times(n => <div key={n} className="square" data-square={n}></div>, 9)}
         </div>
       </div>
@@ -25,20 +25,31 @@ export default withStateHandlers(
     board: times(() => '', 9)
   },
   {
-    checkWinner: () => (board) => {
+    checkWinner: () => (board, turn) => {
       // console.log("coucou");
       const answers = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
-      if (board[0] === board[1] && board[1] === board[2]) {
-        console.log('You Win !');
-      }
+      const checks = map(answer => {
+        if (board[answer[0]] === turn && board[answer[1]] === turn && board[answer[2]] === turn ) {
+          console.log("winner");
+          return (1)
+        }
+      }, answers);
+      console.log(checks);
+
+      const filtered = filter(e => e !== undefined, checks);
+      console.log(filtered);
+
+      return (filtered[0] === 1 ? {gameEnded: true} : {gameEnded: false})
+
     },
-    clicked: () => (e, turn, board, checkWinner) => {
+    clicked: () => (e, turn, board, checkWinner, test) => {
+      console.log(test);
       if (board[e.target.dataset.square] === "") {
         board[e.target.dataset.square] = turn;
         e.target.innerText = turn;
         console.log(board);
-        checkWinner(board);
+        checkWinner(board, turn);
 
         return ({
           turn: turn === 'X' ? 'O' : 'X',
